58115c30be23851bb614f035bad0ee75dba7e1ee
Marcel Maruszak <64kivvi@gmail.com>
2023-12-21 15:26:59 +0100

    add @vueuse/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "nuxt-icon"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "nuxt-icon", "@vueuse/nuxt"],
   app: {
     head: {
       htmlAttrs: {
-        lang: 'pl'
+        lang: "pl",
       },
       title: "Nuxt 1st project",
       meta: [
-        { name: 'description', content: 'This is my first nuxt project. Here I discover basic concepts.' }
-      ]
-    }
+        {
+          name: "description",
+          content:
+            "This is my first nuxt project. Here I discover basic concepts.",
+        },
+      ],
+    },
   },
   runtimeConfig: {
-    currencyKey: process.env.CURRENCY_API_KEY
-  }
-})
+    currencyKey: process.env.CURRENCY_API_KEY,
+  },
+});
